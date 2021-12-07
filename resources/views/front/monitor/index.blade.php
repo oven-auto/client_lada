3e436d1c39823eafbe40aab2496b8f5dcd9d779e
oven-auto <75828618+oven-auto@users.noreply.github.com>
2021-12-07 16:42:18 +0300

    Update index.blade.php

--- resources/views/front/monitor/index.blade.php
+++ resources/views/front/monitor/index.blade.php
@@ -5,19 +5,17 @@
 	<canvas id="canvas" style="z-index:999;width:100%;height: 100vh;position: absolute;top: 0px;left: 0px;"></canvas>
 
 	@include('front.monitor.animation')
-
+    
 	<div class="container-fluid">
-		<div class="row d-flex align-items-center">
-			<div class="col-7">
+		<div style="width: 40%;">
 				<img src="{{asset('storage/'.$issue->car->img)}}" id="car">
-			</div>
+		</div>
 
-			<div class="col-5">
+		<div style="width: 50%; float:left; clear: right;>
 				<h1>Уважаемый</h1>
 				<h1>{{$issue->full_name}}</h1>
 				<h1>Поздравляем с Вашей новой</h1>
 				<h1>{{$issue->car->name}}!</h1>
-			</div>
 		</div>	
 	</div>
 
